9f01595b55d638ebbece55e5762e3a7c3b2bdcff
FannarLevy <fan.levy@gmail.com>
2015-12-06 19:07:29 +0000

    fix git merge issue

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Configure error handling to exit on any non-zero values
-# See SO thread for further info
+# See SO thread for further info:
 # http://stackoverflow.com/questions/821396/aborting-a-shell-script-if-any-command-returns-a-non-zero-value
 set -e
 set -o pipefail
+
 
 echo Cleaning...
 rm -rf ./dist
